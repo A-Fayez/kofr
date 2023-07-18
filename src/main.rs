f195ffc43d8ac0823cde70de2dca323b50cdf4b3
Ahmed Fayez <ahmedf.higgy@gmail.com>
2023-07-19 02:45:26 +0300

    E: constructing vector of connector types from a serde_json::value

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::env;
+use std::fmt;
 
 fn main() -> Result<(), ureq::Error> {
     let uri = env::var("CONNECT_URI").expect("env var CONNECT_URI not found");
-    let connectors = ureq::get(&uri)
+    let connectors: serde_json::Value = ureq::get(&uri)
         .set("Accept", "application/json")
         .call()?
-        .into_string()?;
+        .into_json()?;
 
-    println!("{}", connectors);
+    let mut connectors_vec: Vec<Connector> = Vec::new();
+    let test = connectors.as_array().unwrap();
+
+    for (i, c) in connectors.as_array().unwrap().into_iter().enumerate() {
+        let connector_name: String = serde_json::from_value(*c.get(i).unwrap()).unwrap();
+        connectors_vec.push(Connector {
+            name: connector_name,
+        });
+    }
+
+    for c in connectors_vec {
+        println!("{}", c);
+    }
 
     Ok(())
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct Connector {
     name: String,
+}
+
+impl fmt::Display for Connector {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
 }
