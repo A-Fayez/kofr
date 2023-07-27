da376f316fb47b9e613b58678604afc771fdad6a
Ahmed Fayez <ahmedf.higgy@gmail.com>
2023-07-27 03:15:41 +0300

    defining custom error type

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, KofrError>;
+
+#[derive(Error, Debug)]
+pub enum KofrError {
+    #[error("No connector with name: {0} was found")]
+    ConnectorNotFound(String),
+    #[error("could not deserialize the json returned")]
+    DeserializeConnectorError(#[from] serde_json::Error),
+    #[error("error while requesting uri, {0}")]
+    ApiError(#[from] ureq::Error),
+    #[error("{0}")]
+    IoError(#[from] std::io::Error),
+}
