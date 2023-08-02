3ab01faea233c59b6ad48a2868824dc52b87563b
Ahmed Fayez <ahmedf.higgy@gmail.com>
2023-08-03 00:57:21 +0300

    refactor to returning anyhow::Result on fallible functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@ use serde::{Deserialize, Serialize};
 use std::fmt;
 use ureq::Agent;
 
-use error::Result;
+use anyhow::{Result, Context};
 
 pub fn get_all_connectors(agent: &Agent, uri: &str) -> Result<Vec<Connector>> {
     let connectors: Vec<Connector> = agent
         .get(uri)
         .set("Accept", "application/json")
-        .call()?
-        .into_json()?;
+        .call().with_context(|| format!("Failed sending request to {}", &uri))?
+        .into_json().with_context(|| format!("Could not parse response returned from {}/connectors", &uri))?;
 
     Ok(connectors)
 }
