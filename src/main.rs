802fa0d0e99083bbfca665c5bc45a3d5f83f8f67
Ahmed Fayez <ahmedf.higgy@gmail.com>
2023-08-13 00:54:37 +0300

    Added basic cli framework and implements kofr list command

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use ureq::Agent;
 
 mod error;
 use anyhow::Result;
-
+use clap::{Args, Parser, Subcommand};
 use kofr::{Client, Config};
 
 fn main() -> Result<()> {
@@ -20,11 +20,37 @@ fn main() -> Result<()> {
         connect_uri: (uri.to_owned()),
     });
 
-    let connectors_vec = client.list_connectors()?;
+    let cli = Cli::parse();
 
-    for c in &connectors_vec {
-        dbg!(c);
+    match &cli.command {
+        Commands::List(list) => list.run(client)?,
     }
 
     Ok(())
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// lists active connectors in current cluster
+    #[clap(alias = "ls")]
+    List(List),
+}
+
+#[derive(Args, Debug)]
+struct List {}
+
+impl List {
+    fn run(&self, connect_client: Client) -> Result<()> {
+        let connectors = connect_client.list_connectors()?;
+        for c in &connectors {
+            println!("{}", c.0)
+        }
+        Ok(())
+    }
 }
